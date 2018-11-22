99eaa34b2d4817c3ab7cbbdccf3723323d7a04d4
Sam Wu <swu@nait.ca>
2018-11-22 12:51:18 -0700

    Initial Commit

---
+++ dmit2015-fall2018term-assignment05-client-start/src/main/java/dmit2015/soap/ReadMe.java
@@ -0,0 +1,6 @@
+package dmit2015.soap;
+
+public class ReadMe {
+	// TODO: Use either the jax-ws maven-plugin to generated the XML Web Services classes into this package
+	// or use the wsimport command to generate the source files for the XML Web Services and then copy them into this package.
+}
